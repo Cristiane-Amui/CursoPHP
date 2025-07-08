068fa1b05b960f526798e93287dcf41931e4e3b2
Cristiane-Amui <cristiane.amui@gmail.com>
2025-07-08 12:49:18 -0300

    mais desafios

---
+++ Desafio05/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 05</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        <h1>Analisador de número real</h1>
+        <form action="desafio.php" method="get">
+            <label for="numb">Número Real</label>
+            <input type="number" name="numb" id="idnumb">
+            <input type="submit" value="Analisar">
+        </form>
+    </section>
+</body>
+</html>
