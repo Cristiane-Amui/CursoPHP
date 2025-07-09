0e81fa0a8b08998db0992a06413de9af06e6a244
Cristiane-Amui <cristiane.amui@gmail.com>
2025-07-09 10:41:16 -0300

    ok

--- Desafio04/teste.php
+++
@@ -1,9 +0,0 @@
-<pre>
-<?php 
-    $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'07-02-2025\'&@dataFinalCotacao=\'07-09-2025\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
-
-    $dados = json_decode(file_get_contents($url), true);
-
-    var_dump($dados);
-?>
-</pre>
